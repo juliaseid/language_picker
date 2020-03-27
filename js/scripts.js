cc013010679bf21658dcaa6f279f2b627fc3865b
Julia Seidman <juliaseid@gmail.com>
2020-03-27 11:31:40 -0700

    fix bracket issues in scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,25 @@
-$((document).ready(function() {
-  ($("#userInfo").submit(function(event) {
+$(document).ready(function() {
+  $("#userInfo").submit(function(event) {
     var firstName=$("input#firstName").val();
-    $(".firstName").empty().append(firstName);
-    $("#quizView").show();
-    event.preventDefault();
+    var email=$("input#email").val();
+    
+    if (email) {
+      $(".firstName").empty().append(firstName);
+      $("#quizView").show();
+      event.preventDefault();
+    }
+    else {
+      alert("Please enter a valid email address!")
+    };
   });
 
-  ($("#quiz").submit(function(event) {
+  $("#quiz").submit(function(event) {
     $(".firstName").empty().append(firstName);
     $("#results#resultsTeaser").show();
     event.preventDefault();
   });
 
-  ($("#resultsButton").click(function(event) {
+  $("#resultsButton").click(function(event) {
     $("#realResults").show();
     event.preventDefault();
   });
