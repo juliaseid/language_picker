ed72fe51c8b9973a0de5a4ec2d8c7d00a902b3b5
Julia Seidman <juliaseid@gmail.com>
2020-03-27 23:37:38 -0700

    Move quiz logic inside document.ready function

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+$(document).ready(function() {
+  
 var Q1=$("input:radio[name=apple]:checked").val()
 var Q2=$("input:radio[name=product]:checked").val()
 var Q3=$("input:radio[name=timeFrame]:checked").val()
@@ -20,11 +22,10 @@ function chooseLang(Q1, Q2, Q3, Q4, Q5) {
   }
 return languageRec
 }
-
-function 
-}
-
-$(document).ready(function() {
+  
+  
+  
+  
   $("#userInfo").submit(function(event) {
     var firstName=$("input#firstName").val();
     var email=$("input#email").val();
@@ -49,19 +50,21 @@ $(document).ready(function() {
   });
 
   $("#resultsButton").click(function(event) {
-      if (chooseLang(Q1, Q2, Q3, Q4, Q5)==="Swift") {
+    var langRec=chooseLang(Q1, Q2, Q3, Q4, Q5);
+    var selLangDesc=null;
+      if (langRec==="Swift") {
         selLangDesc="If you love Apple devices, and like the idea of developing apps that could potentially be used by millions of consumers, Swift could be a great language to begin your coding journey.  It is a proprietary language for Apple devices, so it is optimized for the kind of consumer-oriented design Apple is known for!"
       }
-      else if (chooseLang(Q1, Q2, Q3, Q4, Q5)==="Python") {
+      else if (langRec==="Python") {
         selLangDesc="Python is most commonly used in two areas: writing the code that controls web-enabled devices (IoT) for consumer and business applications, and managing and processing large data sets, such as in scientific, financial, and machine learning settings.  Its clear syntax makes it easy to begin, but its optimization for handling mathematical operations means it can be used for extremely complex problems."
       }
-      else if (chooseLang(Q1, Q2, Q3, Q4, Q5)==="Java") {
+      else if (langRec==="Java") {
         selLangDesc="Java is a versatile language that is used in a wide range of applications, but is especially popular for large-scale enterprise applications, such as corporate websites and proprietary software for large, complex projects.  It is not as mobile-friendly as Swift or JS, but is much easier to adapt for consumer-facing applications than Python, and has the stability and scale to meet the needs of large, multi-faceted projects."
       }
-      else if (chooseLang(Q1, Q2, Q3, Q4, Q5)==="JavaScript") {
+      else if (langRec==="JavaScript") {
         selLangDesc="JavaScript is a great place to begin if you're not sure where you want to go!  It is mobile-friendly, but not platform-specific, can interact well with databases, and has a very broad and deep code base to support your work in virtually any project you might take on.  It might not be the very best at anything, but it's relatively easy to learn and extremely versatile."
-      }
-    $("#bestLanguage").empty().append(languageRec);
+      };
+    $("#bestLanguage").empty().append(langRec);
     $("languageDesc").empty().append(selLangDesc);
     $("#realResults").show();
     event.preventDefault();
